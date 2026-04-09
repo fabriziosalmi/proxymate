148915d1ebb952a27db6b3677f04c65812b4bfe1
Fabrizio Salmi <fabrizio.salmi@gmail.com>
2026-04-09 16:08:18 +0200

    Initial Commit

---
+++ proxymate/proxymateApp.swift
@@ -0,0 +1,17 @@
+//
+//  proxymateApp.swift
+//  proxymate
+//
+//  Created by Fabrizio Salmi on 09/04/26.
+//
+
+import SwiftUI
+
+@main
+struct proxymateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
